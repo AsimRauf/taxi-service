11fb669b9effc026d655edbc2241de5ff4c50d37
Asim Rauf <idmery77@gmail.com>
2025-02-16 09:08:34 +0500

    Fix alert for form submission and type issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   // Default to 'nl' as the initial language
-  const defaultLocale = 'nl'
   
   return (
     <Html>
