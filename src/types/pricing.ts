fd9e4d6c4d0ef4199bc3b8b0fe614bd992fb6caf
Asim Rauf <idmery77@gmail.com>
2025-04-29 00:08:00 +0500

    feat: Remove sedan option from FixedPrice and PriceResult interfaces

--- src/types/pricing.ts
+++ src/types/pricing.ts
@@ -1,17 +1,15 @@
 export interface FixedPrice {
-    sedan: number;
     stationWagon: number;
     bus: number;
 }
 
-export interface FixedRouteMap {
+export type FixedRouteMap = {
     [source: string]: {
         [destination: string]: FixedPrice;
     };
-}
+};
 
 export interface PriceResult {
-    sedan: number;
     stationWagon: number;
     bus: number;
     isFixedPrice: boolean;
