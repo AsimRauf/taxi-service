51b822985adee9109e7005abbdb7b833a0743b9f
Asim Rauf <idmery77@gmail.com>
2026-01-02 13:57:00 +0500

    refactor: Rearrange Head and body structure in _document.tsx for clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,14 +7,18 @@ export default function Document() {
         <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favicon.svg?v=2" />
         <link rel="shortcut icon" href="/favicon.svg?v=2" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
         <script type="text/javascript">
           {`
             var sc_project=13194699; 
             var sc_invisible=1; 
-            var sc_security="4caffc9e"; 
+            var sc_security="4caffc9e";
           `}
         </script>
-        <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
+        <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js"></script>
         <noscript>
           <div className="statcounter">
             <a title="Web Analytics Made Easy - Statcounter" href="https://statcounter.com/" target="_blank" rel="noreferrer">
@@ -22,10 +26,6 @@ export default function Document() {
             </a>
           </div>
         </noscript>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
       </body>
     </Html>
   );
