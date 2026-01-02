ee24f021dd582def627a7fecfc0589a68635a5ec
Asim Rauf <idmery77@gmail.com>
2026-01-02 13:54:28 +0500

    refactor: Rearrange script and noscript tags in _document.tsx for better structure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,25 +7,25 @@ export default function Document() {
         <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favicon.svg?v=2" />
         <link rel="shortcut icon" href="/favicon.svg?v=2" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
         <script type="text/javascript">
           {`
             var sc_project=13194699; 
             var sc_invisible=1; 
-            var sc_security="4caffc9e";
+            var sc_security="4caffc9e"; 
           `}
         </script>
         <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
         <noscript>
           <div className="statcounter">
-            <a title="web counter" href="https://statcounter.com/" target="_blank" rel="noreferrer">
-              <img className="statcounter" src="https://c.statcounter.com/13194699/0/4caffc9e/1/" alt="web counter" referrerPolicy="no-referrer-when-downgrade" />
+            <a title="Web Analytics Made Easy - Statcounter" href="https://statcounter.com/" target="_blank" rel="noreferrer">
+              <img className="statcounter" src="https://c.statcounter.com/13194699/0/4caffc9e/1/" alt="Web Analytics Made Easy - Statcounter" referrerPolicy="no-referrer-when-downgrade" />
             </a>
           </div>
         </noscript>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
